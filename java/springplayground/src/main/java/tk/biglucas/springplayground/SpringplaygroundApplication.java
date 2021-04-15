143b75fb97b0f5bcb0a509d160681a892d3da297
lucasew <lucas59356@gmail.com>
2021-04-14 21:19:24 -0300

    java/springplayground: init
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ java/springplayground/src/main/java/tk/biglucas/springplayground/SpringplaygroundApplication.java
@@ -0,0 +1,13 @@
+package tk.biglucas.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringplaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringplaygroundApplication.class, args);
+	}
+
+}
---
+++ java/springplayground/src/test/java/tk/biglucas/springplayground/SpringplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.biglucas.springplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
