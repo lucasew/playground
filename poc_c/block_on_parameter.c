c2ad292b9a619d4f05c5c8879c8200831e3d2b77
lucasew <lucas59356@gmail.com>
2021-07-10 20:17:03 -0300

    poc_c/block_on_parameter: at least I tried
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ poc_c/block_on_parameter.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+// NOTE: Does not work
+
+int sum(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int test = {
+        int n = 2;
+        return n;
+    };
+    int res = sum(
+        {int n = 2;return n;},
+        {int n = 2; return n;}
+    );
+    return 0;
+}
