0186796fa18124b069370a292b3c1db137d29318
lucasew <lucas59356@gmail.com>
2022-01-25 08:44:09 -0300

    example python cgi
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ python_cgi/cgi-bin/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+print("Content-Type: text/html\n")
+print('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>')
+print("<h1>Hello, world</h1>")
+print("</body></html>")
