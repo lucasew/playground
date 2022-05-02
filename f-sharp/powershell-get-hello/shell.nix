21d6725247b2a4af28be7673ead4a1931359d1c2
lucasew <lucas59356@gmail.com>
2022-05-01 22:19:46 -0300

    add example using fsharp to create a powershell library
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ f-sharp/powershell-get-hello/shell.nix
@@ -0,0 +1,10 @@
+{pkgs ? import <nixpkgs> {}
+}:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    dotnet-sdk
+    fsharp
+    powershell
+  ];
+}
+
---
+++ f-sharp/powershell-get-hello/src/PSModule/Library.fs
@@ -0,0 +1,17 @@
+﻿namespace PSModule
+
+open System.Management.Automation
+
+[<Cmdlet("Get", "Hello")>]
+type GetHelloCmdlet () =
+    inherit PSCmdlet ()
+
+    [<Parameter(HelpMessage = "Name to greet")>]
+    member val Name : string = "" with get, set
+
+    override this.EndProcessing () =
+        let text = match this.Name with
+                   | "" -> "Hello, world!"
+                   | name -> "Hello, "+ name + "!"
+        this.WriteObject ( text )
+        base.EndProcessing ()
