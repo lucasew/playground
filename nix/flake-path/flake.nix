939846dfa4c72f2e93732202c9f3883439f19d77
lucasew <lucas59356@gmail.com>
2022-06-05 11:53:46 -0300

    nix/flake-path: init
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/flake-path/flake.nix
+++ nix/flake-path/flake.nix
@@ -2,14 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = self: 
+  outputs = {nixpkgs, ...} @ self: 
   let
-    pkgs = import self.nixpkgs {};
+    pkgs = import nixpkgs { inherit system; };
     inherit (builtins) concatStringsSep attrValues mapAttrs;
+    system = "x86_64-linux";
   in {
-    devShells.default = pkgs.mkShell {
+    devShells.${system}.default = pkgs.mkShell {
       shellHook = ''
-        ${concatStringsSep ":" (
+        NIX_PATH=${concatStringsSep ":" (
           attrValues (
             mapAttrs (k: v: "${k}=${v}") self
           )
