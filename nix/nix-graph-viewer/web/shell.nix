1f7151321fbb9a318ee702282c843cbf9230c928
lucasew <lucas59356@gmail.com>
2022-12-29 19:11:21 -0300

    nix-graph-viewer: moved to own repo
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/nix-graph-viewer/web/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  shellHook = ''
-    PATH="$PATH:$(pwd)/node_modules/.bin"
-  '';
-  buildInputs = with pkgs; [
-    nodejs
-  ];
-}
--- nix/nix-graph-viewer/web/src/stores/data.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const dataStore = writable({})
-
-window.setData = (newData) => {
-    dataStore.set(newData)
-}
