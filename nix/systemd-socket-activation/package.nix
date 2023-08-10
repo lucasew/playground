635db6fce27f82edc2ffd1ff82998d63d3598163
lucasew <lucas59356@gmail.com>
2023-08-09 23:58:03 -0300

    nix/systemd-socket-activation: I think I found a bug in systemd
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- nix/systemd-socket-activation/package.nix
+++ nix/systemd-socket-activation/package.nix
@@ -12,18 +12,23 @@ import "${path}/nixos/tests/make-test-python.nix" ({pkgs, ...}: {
       systemd.sockets.demo-server = {
         socketConfig = {
           ListenStream = 80;
-          Service = "demo-server.service";
+          # Service = "demo-server.service";
+        };
+        unitConfig = {
+          BindsTo = [ "demo-server.service" ];
         };
         wantedBy = [ "sockets.target" ];
       };
       systemd.services.demo-server = {
-        path = [ python3 ];
+        script = "${python3}/bin/python ${./payload.py} --out-dir $STATE_DIRECTORY";
+        unitConfig = {
+          Requires = [ "demo-server.socket" ];
+          After = [ "network.target" "demo-server.socket" ];
+        };
         serviceConfig = {
           StateDirectory = "demo_server";
+          Restart = "always";
         };
-        script = ''
-          python ${./payload.py} --out-dir $STATE_DIRECTORY
-        '';
       };
     };
 
@@ -34,14 +39,29 @@ import "${path}/nixos/tests/make-test-python.nix" ({pkgs, ...}: {
 
   testScript = ''
     start_all()
+
+    # not running at first
     server.succeed("[ ! -f /var/lib/demo_server/init ]")
     server.succeed("[ ! -f /var/lib/demo_server/request0 ]")
     client.sleep(2)
+
+    # wake up
     client.succeed("curl server")
     client.sleep(2)
+
+    # confirm it's started
     server.succeed("[ -f /var/lib/demo_server/init ]")
     server.succeed("[ -f /var/lib/demo_server/request0 ]")
     client.succeed("curl server")
+
+    # confirm that's not starting another instance
     server.succeed("[ -f /var/lib/demo_server/request1 ]")
+    client.sleep(10)
+
+    # confirm that's stopped for inactivity
+    server.succeed("[ -f /var/lib/demo_server/stop ]")
+
+    # check if the service can be restarted
+    client.succeed("curl server")
   '';
 })
--- nix/systemd-socket-activation/payload.py
+++ nix/systemd-socket-activation/payload.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import os
 import logging
 import socket
+from threading import Thread
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -15,6 +16,7 @@ parser.add_argument("-o", "--out-dir", type=Path, dest='out_dir')
 
 args = parser.parse_args()
 
+logger.debug(f"env: {os.environ.values()}")
 # assert os.getenv("LISTEN_FDS") == "3"
 logger.info("Starting")
 
@@ -36,7 +38,21 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         logger.info("Receiving request")
         (args.out_dir / f"request{next(reqs_iter)}").write_text("ok")
 
-with socketserver.TCPServer(None, Handler, bind_and_activate=False) as httpd:
-    httpd.socket = socket.socket(family=httpd.address_family, type=httpd.socket_type, fileno=3)
-    httpd.serve_forever()
+class CustomTCPServer(socketserver.TCPServer):
+    def is_continue(self):
+        return not self._BaseServer__shutdown_request
+
+    def handle_timeout(self):
+        Thread(target=lambda: self.shutdown()).start()
+        logger.debug('trigger timeout, shutdown')
+
+with CustomTCPServer(None, Handler, bind_and_activate=False) as httpd:
+    httpd.socket = socket.socket(family=httpd.address_family, type=httpd.socket_type, fileno=int(os.getenv("LISTEN_FDS_FIRST_FD") or 3))
+    httpd.socket.settimeout(3)
+    while httpd.is_continue():
+        logger.debug('checking if continue')
+        httpd.handle_request()
+
 logger.info("Stopping server")
+(args.out_dir / "stop").write_text("ok")
+exit(0)
