cc9087e3a0178cf959301f9c10654a9994c546a6
lucasew <lucas59356@gmail.com>
2023-09-02 14:11:50 -0300

    20230901-mediakit:  iterando pra o novo approach
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- PROJETOS/20230901-mediakit/mediakit_project/utils/feed/rss.py
+++ PROJETOS/20230901-mediakit/mediakit_project/utils/feed/rss.py
@@ -37,63 +37,27 @@ def extract(repo: FeedRepository):
         for enclosure in entry['enclosures']:
             if enclosure.type.startswith("audio"):
                 audio_url = enclosure['href']
+        icon = None
+        try:
+            icon = entry['image']['href']
+        except KeyError:
+            pass
+
+        article_content = ""
+        try:
+            article_content = entry['content'][0]['value']
+        except KeyError:
+            pass
 
         repo.update_post(
             entry['link'],
             published_time_unix,
-            article_content=entry.get('content'),
+            article_content=article_content,
             audio_url=audio_url,
             author=entry.get('author'),
             summary=entry['summary'],
             title=entry.get('title'),
+            icon=icon
         )
-
-
-
-
-
-
-def refresh_feeds():
-    feed_dir = get_root_dir()
-    feeds = []
-    for feed in feed_dir.iterdir():
-        if not feed.is_dir():
-            continue
-        if feed.name.startswith("__"):
-            continue
-        with utils.ContextJSON(feed / "__meta__.json") as d:
-            last_updated = d['_last_updated']
-            url = d['url']
-            feeds.append(dict(last_updated=last_updated, url=url))
-    feeds.sort(key=lambda x: x['last_updated'])
-    for feed in feeds:
-        yield fetch_rss(feed['url'])
-
-
-def fetch_rss(url: str):
-    logger.info(_("fetching '{url}'").format(url=url))
-    data = feedparser.parse(url)
-    feed_dir = get_feed_dir(url)
-
-    head_node = data['feed']
-    head_data = dict(
-        url=url,
-        subtitle=head_node.get('subtitle'),
-        title=head_node.get('title'),
-    )
-    with utils.ContextJSON(feed_dir / "__meta__.json") as d:
-        d['url'] = url
-        d['title'] = head_node.get('title_detail')
-        d['subtitle'] = head_node.get('subtitle')
-        d['image'] = head_node.get('image')
-        d['_last_updated'] = int(time.time())
-
-    for entry in data['entries']:
-        published_time = entry['published_parsed']
-        with utils.ContextJSON(feed_dir / f"{timestamp_to_unix(published_time)}.json") as d:
-            for k in ["links", "summary", "title", "comments", "enclosures", "published", "expired"]:
-                d[k] = entry.get(k)
-
-
 
 
