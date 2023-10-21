9a4f9ac4211b413dbf49c2ccbc8a48c973b168ff
lucasew <lucas59356@gmail.com>
2023-10-21 13:00:16 -0300

    revealjs/demo-reveal-svelte: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ revealjs/demo-reveal-svelte/src/routes/deck/[slug]/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import 'reveal.js/dist/reveal.css';
+    import 'reveal.js/dist/reset.css';
+    import Reveal            from 'reveal.js';
+
+    import { onMount, tick } from 'svelte';
+
+    onMount(async () => {
+        console.log("reveal")
+        const revealConfig = {
+            controls: true,
+            progress: true,
+            center: true,
+            hash: true,
+            plugins: []
+        }
+        await tick();
+        const deck = new Reveal(reveal);
+        deck.initialize();
+      });
+
+</script>
+
+<div class="reveal">
+    <div class="slides">
+        <slot/>
+    </div>
+</div>
---
+++ revealjs/demo-reveal-svelte/src/routes/deck/teste/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Slide from '$lib/Slide.svelte';
+    console.log("aaaa")
+</script>
+
+<Slide>
+    <h1>Teste</h1>
+</Slide>
