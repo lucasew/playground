f85aeabdc0450072890883a67a36def6e650792a
lucasew <lucas59356@gmail.com>
2023-10-21 13:25:54 -0300

    revealjs/demo-reveal-svelte: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ revealjs/demo-reveal-svelte/src/routes/deck/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import 'reveal.js/dist/reveal.css';
+    import 'reveal.js/dist/reset.css';
+    import 'reveal.js/dist/theme/black.css';
+    import 'reveal.js/plugin/highlight/monokai.css';
+
+    import Highlight from 'reveal.js/plugin/highlight/highlight';
+    import Markdown from 'reveal.js/plugin/markdown/markdown';
+    import MathReveal from 'reveal.js/plugin/math/math';
+    import RevealNotes from 'reveal.js/plugin/notes/notes';
+    import RevealSearch from 'reveal.js/plugin/search/search';
+
+    import Reveal from 'reveal.js';
+
+    import { onMount, tick } from 'svelte';
+
+    onMount(async () => {
+        console.log("reveal")
+        const revealConfig = {
+            controls: true,
+            progress: true,
+            center: true,
+            hash: true,
+            plugins: [
+                Highlight,
+                Markdown,
+                MathReveal,
+                RevealNotes,
+                RevealSearch
+            ]
+        }
+        await tick();
+        const deck = new Reveal(revealConfig);
+        deck.initialize();
+      });
+
+</script>
+
+<div class="reveal">
+    <div class="slides">
+        <slot/>
+    </div>
+</div>
--- revealjs/demo-reveal-svelte/src/routes/deck/teste/+page.svelte
+++ revealjs/demo-reveal-svelte/src/routes/deck/teste/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
     import Slide from '$lib/Slide.svelte';
+    import Code from '$lib/Code.svelte';
     console.log("aaaa")
 </script>
 
 <Slide>
     <h1>Teste</h1>
+</Slide>
+<Slide>
+    <h1>Bla bla bla</h1>
+    <Code trim=true lineNumbers=true>
+        console.log("hello")
+    </Code>
 </Slide>
