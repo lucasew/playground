463d9b73d759689c7e35fcad8cadd6aa34e29d47
lucasew <lucas59356@gmail.com>
2023-10-29 10:54:19 -0300

    c/vulkan-triangle: fixed function state
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- c/vulkan-triangle/main.c
+++ c/vulkan-triangle/main.c
@@ -454,6 +454,114 @@ int main(int argc, char* argv[]) {
 
     VkPipelineShaderStageCreateInfo shaderStages[] = {vertexShaderStageCreateInfo, fragmentShaderStageCreateInfo};
 
+    VkDynamicState dynamicStates[] = {
+        VK_DYNAMIC_STATE_VIEWPORT,
+        VK_DYNAMIC_STATE_SCISSOR,
+    };
+
+    VkPipelineDynamicStateCreateInfo dynamicStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+        .dynamicStateCount = 2,
+        .pDynamicStates = dynamicStates
+    };
+
+    VkPipelineVertexInputStateCreateInfo vertexInputStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+        .vertexBindingDescriptionCount = 0,
+        .pVertexBindingDescriptions = NULL, // optional
+        .vertexAttributeDescriptionCount = 0,
+        .pVertexAttributeDescriptions = NULL, // optional
+    };
+
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+        .topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+        .primitiveRestartEnable = VK_FALSE
+    };
+
+    VkViewport viewport = {
+        .x = 0.0f,
+        .y = 0.0f,
+        .width = (float) swapChainExtent.width,
+        .height = (float) swapChainExtent.height,
+        .minDepth = 0.0f,
+        .maxDepth = 1.0f
+    };
+
+    VkOffset2D scissorOffset = {0, 0};
+    VkRect2D scissor = {
+        .offset = scissorOffset,
+        .extent = swapChainExtent
+    };
+
+    VkPipelineViewportStateCreateInfo viewportStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+        .viewportCount = 1,
+        .pViewports = &viewport,
+        .scissorCount = 1,
+        .pScissors = &scissor
+    };
+
+    VkPipelineRasterizationStateCreateInfo rasterizationStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .lineWidth = 1.0f,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f, // optional
+        .depthBiasClamp = 0.0f, // optional
+        .depthBiasSlopeFactor = 0.0f, // optional
+    };
+
+    VkPipelineMultisampleStateCreateInfo multisampleStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .sampleShadingEnable = VK_FALSE,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+        .minSampleShading = 1.0f, // optional
+        .pSampleMask = NULL, // optional
+        .alphaToCoverageEnable = VK_FALSE, // optional
+        .alphaToOneEnable = VK_FALSE // optional
+    };
+
+    VkPipelineColorBlendAttachmentState colorBlendAttachment = {
+        .colorWriteMask =
+              VK_COLOR_COMPONENT_R_BIT
+            | VK_COLOR_COMPONENT_G_BIT
+            | VK_COLOR_COMPONENT_B_BIT
+            | VK_COLOR_COMPONENT_A_BIT,
+        .blendEnable = VK_FALSE,
+        .srcColorBlendFactor = VK_BLEND_FACTOR_ONE, // optional
+        .dstColorBlendFactor = VK_BLEND_FACTOR_ZERO, // optional
+        .colorBlendOp = VK_BLEND_OP_ADD, // optional
+        .srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE, // optional
+        .dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO, // optional
+        .alphaBlendOp = VK_BLEND_OP_ADD // optional
+    };
+
+    VkPipelineColorBlendStateCreateInfo colorBlendStateCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+        .logicOpEnable = VK_FALSE,
+        .logicOp = VK_LOGIC_OP_COPY,
+        .attachmentCount = 1,
+        .pAttachments = &colorBlendAttachment,
+        .blendConstants = { 0.0f, 0.0f, 0.0f, 0.0f }
+    };
+
+    VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo = {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+        .setLayoutCount = 0, // optional
+        .pSetLayouts = NULL, // optional
+        .pushConstantRangeCount = 0, // optional
+        .pPushConstantRanges = NULL
+    };
+
+    VkPipelineLayout pipelineLayout;
+    if (vkCreatePipelineLayout(device, &pipelineLayoutCreateInfo, NULL, &pipelineLayout) != VK_SUCCESS) {
+        fprintf(stderr, "vulkan: não foi possivel criar o pipeline layout\n");
+    }
 
 
     // Paused at: https://vulkan-tutorial.com/en/Drawing_a_triangle/Graphics_pipeline_basics/Fixed_functions
@@ -462,6 +570,8 @@ int main(int argc, char* argv[]) {
         glfwPollEvents();
     }
     fprintf(stderr, "E agui\n");
+
+    vkDestroyPipelineLayout(device, pipelineLayout, NULL);
 
     vkDestroyShaderModule(device, vertexShaderModule, NULL);
     vkDestroyShaderModule(device, fragmentShaderModule, NULL);
