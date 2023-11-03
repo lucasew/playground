f78e1106d8f5ac41b11405d49d7bd2e995ea2f33
lucasew <lucas59356@gmail.com>
2023-11-02 21:46:08 -0300

    c/vulkan.cu: checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- c/vulkan.cu/shell.nix
+++ c/vulkan.cu/shell.nix
@@ -1,5 +1,10 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; }}:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [ cudatoolkit ];
+  buildInputs = with pkgs; [
+    cudatoolkit
+    rocmPackages.clr
+    rocmPackages.hipify
+    clang
+  ];
 }
