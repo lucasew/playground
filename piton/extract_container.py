87d1a50004b5d905dff88525debf1f29d6a69aa7
lucasew <lucas59356@gmail.com>
2024-06-04 21:31:55 -0300

    isso mesmo que vc leu, eu fiz um script pra baixar e extrair imagem docker
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- piton/extract_container.py
+++ piton/extract_container.py
@@ -7,11 +7,15 @@ import urllib
 from urllib.request import build_opener, Request,urlopen, _opener as __opener
 from urllib.error import HTTPError
 from pprint import pprint
+import tarfile
+import tempfile
 
 parser = ArgumentParser()
 
 # no namespace == library, ex: nginx -> library/nginx
-parser.add_argument('image') 
+parser.add_argument('image') # example: library/hello-world
+parser.add_argument('--os', default='linux')
+parser.add_argument('--arch', default='amd64')
 parser.add_argument('output')
 
 args = parser.parse_args()
@@ -31,29 +35,27 @@ def urlopen(*args,**kwargs):
     # return opener.open(*args, **kwargs)
 
 
-def fetch_image(image='library/hello-world', os='linux', architecture='amd64'):
-    token = json.load(urlopen(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image}:pull"))['token']
-    
-    urlopen("https://registry.hub.docker.com/v2")
-    req = f"https://registry.hub.docker.com/v2/repositories/{image}/tags"
+token = json.load(urlopen(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{args.image}:pull"))['token']
+
+def fetch_image_data():
+    # urlopen("https://registry.hub.docker.com/v2")
+    req = f"https://registry.hub.docker.com/v2/repositories/{args.image}/tags"
     with urlopen(req) as res:
         # print(res.headers['WWW-Authenticate'])
         data = json.load(res)
         for result in data['results']:
             for image_data in result['images']:
-                if image_data['os'] == os and image_data['architecture'] == architecture:
+                if image_data['os'] == args.os and image_data['architecture'] == args.arch:
                     digest = image_data['digest']
-                    req = Request(f"https://registry.hub.docker.com/v2/{image}/manifests/{digest}", headers={
+                    req = Request(f"https://registry.hub.docker.com/v2/{args.image}/manifests/{digest}", headers={
                         "Authorization": f"Bearer {token}",
                         "Accept": "application/vnd.oci.image.manifest.v1+json"
                     })
                     with urlopen(req) as res:
-                        # res_str = res.read().decode('utf-8')
                         manifest = json.load(res)
                         
-                        req = Request(f"https://registry.hub.docker.com/v2/{image}/blobs/{manifest['config']['digest']}", headers={
+                        req = Request(f"https://registry.hub.docker.com/v2/{args.image}/blobs/{manifest['config']['digest']}", headers={
                             "Authorization": f"Bearer {token}",
-                            # "Accept": "application/vnd.oci.image.config.v1+json"
                         })
                         with urlopen(req) as res:
                             print(res.status, res.headers)
@@ -63,7 +65,25 @@ def fetch_image(image='library/hello-world', os='linux', architecture='amd64'):
                         image_data['manifest'] = manifest
                         yield image_data
                             
-
-image = next(fetch_image(image=args.image))
-
+image = next(fetch_image_data())
 pprint(image)
+
+for layer in image['manifest']['layers']:
+    print('layer', layer)
+
+    req = Request(f"https://registry.hub.docker.com/v2/{args.image}/blobs/{layer['digest']}", headers={
+        "Authorization": f"Bearer {token}",
+    })
+    with urlopen(req) as res:
+        blob_size = int(res.getheader('Content-Length'))
+        print(f"Extraindo blob {layer['digest']} ({blob_size}b)")
+        with tempfile.TemporaryFile() as tempfile:
+            while True:
+                data = res.read(16*1024)
+                if not data:
+                    break
+                tempfile.write(data)
+            tempfile.seek(0)
+            with tarfile.open(mode='r', fileobj=tempfile) as layer:
+                layer.extractall(args.output)
+    
