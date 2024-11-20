f891577bc678760e31cad125384b6d1a0ed7738b
lucasew <lucas59356@gmail.com>
2024-11-20 13:21:12 -0300

    got ya
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- piton/flask-app-builder/app/models.py
+++ piton/flask-app-builder/app/models.py
@@ -1,7 +1,12 @@
-from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from flask_appbuilder import Model, expose, has_access, permission_name
+from sqlalchemy import Column, Integer, String, ForeignKey, BLOB, event
+from sqlalchemy.orm import relationship, Mapped
 from flask_appbuilder.models.mixins import FileColumn
+from flask_appbuilder.models.decorators import renders
+from flask import Markup, url_for
+from typing import List
+import time
+import hashlib
 
 """
 
@@ -25,10 +30,40 @@ class FileStorage(Model):
 
     def download(self):
         return Markup(
-            '<a href="'
-            + url_for("ProjectFilesModelView.download", filename=str(self.file))
+            '<a target="_blank" href="'
+            + url_for("ProjectFilesModelView.download", str(self.id))
             + '">Download</a>'
         )
 
     def file_name(self):
         return get_file_original_name(str(self.file))
+
+class DataBlob(Model):
+    hash = Column(String(), unique=True, nullable=False, primary_key=True)
+    data = Column(BLOB, nullable=False)
+
+    usages: Mapped[List['VersionedFileStorage']] = relationship('VersionedFileStorage', back_populates='blob')
+
+@event.listens_for(DataBlob, 'before_insert')
+def add_hash_to_blob(mapper, connect, target):
+    if isinstance(target.data, str):
+        target.data = target.data.encode('utf-8')
+    if not isinstance(target.data, bytes):
+        target.data = target.data.read()
+    hasher = hashlib.sha256(target.data)
+    target.hash = hasher.hexdigest()
+
+
+class VersionedFileStorage(Model):
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), unique=True, nullable=False)
+    version = Column(Integer(), nullable=False, default=lambda: int(time.time()))
+    blob_id = Column(ForeignKey(DataBlob.hash))
+    blob: Mapped['DataBlob'] = relationship('DataBlob', back_populates='usages')
+
+    def download(self):
+        return Markup(
+            '<a target="_blank" href="'
+            + url_for("VersionedFileStorage.download", pk=self.id)
+            + '">Download</a>'
+        )
--- piton/flask-app-builder/app/views.py
+++ piton/flask-app-builder/app/views.py
@@ -1,7 +1,14 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request, make_response
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder import ModelView, ModelRestApi
+from flask_appbuilder.forms import DynamicForm
 from flask_appbuilder.actions import action
+from flask_appbuilder.urltools import get_filter_args
+from flask_appbuilder.baseviews import expose, expose_api
+from flask_appbuilder.security.decorators import has_access_api, has_access, permission_name
+from flask_appbuilder.widgets import FormVerticalWidget
+from wtforms import Form
+from wtforms.fields import FileField
 
 from . import appbuilder, db, models
 
@@ -9,6 +16,84 @@ class CounterApi(ModelRestApi):
     datamodel = SQLAInterface(models.Counter)
 
 appbuilder.add_api(CounterApi)
+
+class FileForm(DynamicForm):
+    file = FileField("Arquivo")
+
+class VersionedFileStorage(ModelView):
+    datamodel = SQLAInterface(models.VersionedFileStorage)
+
+    label_columns = {
+        'name': "Nome do arquivo",
+        'version': 'Versão do arquivo',
+        'blob': 'Arquivo'
+    }
+
+    add_columns = ['file']
+    list_columns = ['name', 'version', 'download']
+    get_columns = ['name', 'version', 'download']
+
+    @expose("/download/<pk>")
+    @has_access
+    def download(self, pk):
+        item = self.datamodel.get(pk)
+        response = make_response(item.blob.data, 200)
+        response.headers['Content-Disposition'] = f"attachment; filename={item.name}"
+        return response
+
+    def _add(self): # https://github.com/dpgaspar/Flask-AppBuilder/blob/fab9013003a41c4e80da04f072201a8c7cc99187/flask_appbuilder/baseviews.py#L1208
+        is_valid_form = True
+        get_filter_args(self._filters, disallow_if_not_in_search=False)
+        exclude_cols = self._filters.get_relation_cols()
+        form = self.add_form.refresh()
+        if request.method == 'POST':
+            self._fill_form_exclude_cols(exclude_cols, form)
+            if form.validate():
+                self.process_form(form, True)
+                item = self.datamodel.obj()
+
+                try:
+                    # print(form.file)
+                    file = form._fields['file']
+
+                    # print(dir(file))
+                    # print(file.short_name)
+                    data = file.raw_data[0]
+                    # print('data', data)
+                    # print('data', dir(data))
+                    # print('data', data.filename)
+                    # print('data', data.stream, type(data.stream))
+
+                    item.name = data.filename
+                    item.blob = models.DataBlob(
+                        data=data.stream.read()
+                    )
+
+                    self.pre_add(item)
+                except Exception as e:
+                    flash(str(e), "danger")
+                    print(e)
+                else:
+                    if self.datamodel.add(item):
+                        pass
+                        self.post_add(item)
+                    flash(*self.datamodel.message)
+                finally:
+                    return None
+            else:
+                is_valid_form = False
+        if is_valid_form:
+            self.update_redirect()
+        return self._get_add_widget(form=form, exclude_cols=exclude_cols)
+
+    add_form = FileForm
+
+appbuilder.add_view(
+    VersionedFileStorage,
+    "Lista arquivos e versões",
+    icon='fa-paperclip',
+    category="Exemplos"
+)
 
 class FileStorageModelView(ModelView):
     datamodel = SQLAInterface(models.FileStorage)
