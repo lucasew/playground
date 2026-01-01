2fc1ac7b4a741f394dcd043d6b9900cf8ecb5c6d
lucasew <lucas59356@gmail.com>
2026-01-01 10:41:37 -0300

    old stuff
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- PROJETOS/20251120-poc-template-go/teste.go
+++ PROJETOS/20251120-poc-template-go/teste.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+)
 
+type Result struct {
+	Major string
+}
+
+type Input struct {
+	Version string
+}
+
+func testTemplate(templ string) string {
+	var buf bytes.Buffer
+
+	tmpl := template.Must(template.New("teste").Funcs(
+		template.FuncMap{
+			"semver": func(args string) Result {
+				return Result{Major: "2"}
+			},
+		},
+	).Parse(templ))
+	tmpl.Execute(&buf, Input{Version: "2.0.0"})
+	return buf.String()
+}
+
+func main() {
+	fmt.Printf("Result no space: '%s'\n", testTemplate(`{{(semver .Version).Major}}`))
+	fmt.Printf("Result space: '%s'\n", testTemplate(`{{(semver .Version) .Major}}`))
 }
